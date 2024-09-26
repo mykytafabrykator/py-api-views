b88566f0cf416ad68dbd67564cc27a06539fcdc4
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-26 18:01:54 +0300

    Refactor serializers

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import (
+    Actor,
+    CinemaHall,
+    Genre,
+    Movie,
+)
 
 
 class MovieSerializer(serializers.Serializer):
@@ -8,9 +13,22 @@ class MovieSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     description = serializers.CharField()
     duration = serializers.IntegerField()
+    actors = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all(),
+        many=True,
+    )
+    genres = serializers.PrimaryKeyRelatedField(
+        queryset=Genre.objects.all(),
+        many=True,
+    )
 
     def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
+        actors = validated_data.pop("actors", [])
+        genres = validated_data.pop("genres", [])
+        movie = Movie.objects.create(**validated_data)
+        movie.actors.set(actors)
+        movie.genres.set(genres)
+        return movie
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
@@ -19,6 +37,33 @@ class MovieSerializer(serializers.Serializer):
         )
         instance.duration = validated_data.get("duration", instance.duration)
 
+        if "actors" in validated_data:
+            instance.actors.set(validated_data.pop("actors", []))
+
+        if "genres" in validated_data:
+            instance.genres.set(validated_data.pop("genres", []))
+
         instance.save()
 
         return instance
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+        read_only_fields = ("id",)
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = "__all__"
+        read_only_fields = ("id",)
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
+        read_only_fields = ("id",)
