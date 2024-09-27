f9fcc7a71df41175484d5987e19c5979c0d9df69
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-09-27 15:48:11 +0300

    Add related names to Movie fields

--- cinema/models.py
+++ cinema/models.py
@@ -38,8 +38,8 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
-    actors = models.ManyToManyField(Actor)
-    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     class Meta:
         ordering = ["title"]
